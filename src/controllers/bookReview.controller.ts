d2fd9de6cd30c3b7e6eee1712d21aef454d400f5
Developer Hash <tech247gb@gmail.com>
2024-07-29 14:09:52 +0530

    Implemented add book review api Implemented edit book review api Implemented delete book review api

--- src/controllers/bookReview.controller.ts
+++ src/controllers/bookReview.controller.ts
@@ -4,7 +4,37 @@ import { makeObjectId } from "../helpers/mongoose.helper";
 import { parseToken } from "../helpers/token.helpers";
 import { getReviews, makeSearchQuery } from "../helpers/bookReview.helper";
 
-/*
+/**
+ * Adds a new book review.
+ *
+ * @param req - Express request object
+ * @param res - Express response object
+ * @returns A JSON response indicating success or failure
+ */
+export const addBookReview = async (req: Request, res: Response) => {
+    try {
+        const { title, author, reviewText, rating ,imageUrl } = req.body
+        // Validate input fields
+        if (!title || !author || !reviewText || !rating) {
+            return res.status(406).send({ message: 'Missing booking review details' })
+        }
+        const userId = parseToken(req.headers)?._id ?? null
+
+        const newReview = new BookReviews({ title, author, reviewText, rating, createdBy: userId ,imageUrl })
+
+        const savedReview = await newReview.save()
+
+        if (!savedReview) {
+            return res.status(500).send({ success: false, message: 'Review not added' })
+        }
+        return res.status(201).send({ success: true, message: 'Review added successfully' })
+
+    } catch (err: any) {
+        console.error("Error on adding Book review:", err.message)
+        res.status(500).json({ error: err.message })
+    }
+}
+/**
  * View all book reviews
  *
  * @param req - Express request object
@@ -26,6 +56,58 @@ export const viewAllBookReviews = async (req: Request, res: Response) => {
     }
 }
 
+/**
+ * Edit and update a book review.
+ *
+ * @param req - Express request object
+ * @param res - Express response object
+ * @returns A JSON response indicating success or failure
+ */
+export const editBookReview = async (req: Request, res: Response) => {
+    try {
+        const { updateObject } = req.body
+        const { id } = req.params
+        const { _id: userId } = parseToken(req.headers)
+
+
+        if (!id) return res.status(402).send({ message: 'Review Id is missing' })
+
+        const objectId = makeObjectId(id)
+        const review = await BookReviews.findOne({ _id: objectId, createdBy: userId }).lean()
+
+        if (!review) {
+            return res.status(401).send({ message: "There is no review for this user." })
+        }
+        await BookReviews.findByIdAndUpdate({ _id: objectId }, { ...updateObject })
+        return res.status(200).send({ message: "Review updated successfully" })
+    } catch (err: any) {
+        console.log("Error occured when updating reviews ", err.message)
+        return res.json({ err: err.message })
+    }
+}
+/**
+ * Delete a book review.
+ *
+ * @param req - Express request object
+ * @param res - Express response object
+ * @returns A JSON response indicating success or failure
+ */
+export const deleteReviewById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const { _id: userId } = parseToken(req.headers)
+        if (!id) return res.status(402).send({ message: 'Review Id is missing' })
+        const reviewByuser = await BookReviews.findOne({ _id: makeObjectId(id), createdBy: userId })
+        if (!reviewByuser) {
+            return res.status(401).send({ message: `You don't have permission to delete this review id : ${id} ` })
+        }
+        await reviewByuser.deleteOne()
+        return res.status(200).send({ message: `Successfully deleted the review with id : ${reviewByuser._id.toString()} ` })
+    } catch (err: any) {
+        console.log("Error on deleting a review :", err.message)
+        return res.json({ err: err.message })
+    }
+}
 /**
  * View a book review by book id..
  *
--- src/routes/bookReviews.route.ts
+++ src/routes/bookReviews.route.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { getBookReviewById, viewAllBookReviews } from "../controllers/bookReview.controller";
+import { addBookReview, deleteReviewById, editBookReview, getBookReviewById, viewAllBookReviews } from "../controllers/bookReview.controller";
+import { verifyToken } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.get("/get-all-reviews",viewAllBookReviews )
 router.get("/get-review/:id",getBookReviewById )
+router.post("/add-review",verifyToken ,addBookReview )
+router.put("/edit-review/:id",verifyToken, editBookReview )
+router.delete("/delete-review/:id",verifyToken ,deleteReviewById )
 
 export default router;
